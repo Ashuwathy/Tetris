3ba3cb6993b5a03eaecc6a3eedef6917d4759410
Ashuwathy <ashuwathyt@gmail.com>
2023-03-19 09:00:30 +0530

    added random colors and call to update grid

--- Board.py
+++ Board.py
@@ -1,5 +1,6 @@
 import pygame
-from constant import cols, rows, blockSize, width, height, white, grey
+import random
+from constant import cols, rows, blockSize, width, height, white, grey, colors
 
 class Board:
     def __init__(self):
@@ -14,16 +15,20 @@ class Board:
 
     def drawGrid(self):
         for y, row in enumerate(self.board):
-            for x, val in enumerate(self.board):
+            for x, val in enumerate(row):
                 rect = pygame.Rect(x * blockSize, y * blockSize, blockSize, blockSize)
+                if val:
+                    pygame.draw.rect(self.screen, random.choice(colors), rect, 0)
+                else:
+                    pygame.draw.rect(self.screen, white, rect, 0)
                 pygame.draw.rect(self.screen, grey, rect, 1)
         pygame.display.update()
 
     def remove_row(self):
         for j, row in enumerate(self.board[::-1]):
             if 0 not in row:
-                del self.board[row]
+                self.board.remove(row)
                 self.board = [[0 for _ in range(cols)]] + self.board
+                self.drawGrid()
             else:
                 break
-        pygame.display.update()
